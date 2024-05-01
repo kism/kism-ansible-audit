d43fb7b0aff38a9083c0adab667d9a46ad1a3cc5
Kieran Gee <kieran.lost.the.game@gmail.com>
2024-05-01 15:35:26 +0800

    better

--- kism-ansible-audit.py
+++ kism-ansible-audit.py
@@ -12,13 +12,12 @@ EXCLUDES = [".venv"]
 DIRECTORY = "/home/kism/src/ansible-playbooks/"
 
 
-def find_name_entries(directory):
+def find_name_entries(directory, excludes):
     """Get list of name tags in yaml files, and their path"""
     name_entries = []
     for root, dirs, files in os.walk(directory):
 
-        dirs[:] = [d for d in dirs if not d.startswith('.')]
-
+        dirs[:] = [d for d in dirs if not d.startswith(".")]
 
         for file in files:
             if file.endswith(".yaml") or file.endswith(".yml"):
@@ -28,52 +27,48 @@ def find_name_entries(directory):
                     if isinstance(yaml_content, list):
                         for entry in yaml_content:
                             if isinstance(entry, dict) and "name" in entry:
-                                name_entries.append({'name': entry["name"], 'path': file_rel_path})
-    return name_entries
+                                name_entries.append({"name": entry["name"], "path": file_rel_path})
 
-
-def remove_excluded_paths(results, excludes):
-    """Return results that's path is in the excluded paths"""
     filtered_results = []
 
     for exclude in excludes:
-        for result in results:
-            print(result)
-            if not fnmatch.fnmatch(result['path'], exclude):
+        for result in name_entries:
+            if not fnmatch.fnmatch(result["path"], exclude):
                 filtered_results.append(result)
 
-    return filtered_results
+    return name_entries
 
-def create_deduplicated_list(results):
+
+def custom_dict_sort(item):
+    """For sorting entries"""
+    return item[1]["count"]
+
+
+def create_final_results(results):
     """Create new data structure for name keys and the files they are in"""
     filtered_results = {}
 
     for result in results:
-        if result['name'] not in filtered_results:
-            filtered_results[result['name']] = {'paths': [result['path']], 'count': 1}
+        if result["name"] not in filtered_results:
+            filtered_results[result["name"]] = {"paths": [result["path"]], "count": 1}
         else:
-            filtered_results[result['name']]['paths'].append(result['path'])
-            filtered_results[result['name']]['count'] += 1
+            filtered_results[result["name"]]["paths"].append(result["path"])
+            filtered_results[result["name"]]["count"] += 1
 
-    return filtered_results
+    filtered_results = {key: value for key, value in filtered_results.items() if value.get("count") != 1}
 
-def custom_dict_sort(item):
-    """For sorting entries"""
-    return item[1]['count']
+    sorted_results = dict(sorted(filtered_results.items(), key=custom_dict_sort))
 
-results = find_name_entries(DIRECTORY)
-results = remove_excluded_paths(results, EXCLUDES)
-
-deduplicated_dict = create_deduplicated_list(results)
-
-# for key, item in deduplicated_dict.items():
-#     print(key, "|", item)
+    return sorted_results
 
 
-# Sort the dictionary based on the 'order' subkey
-sorted_dict = dict(sorted(deduplicated_dict.items(), key=custom_dict_sort))
+results = find_name_entries(DIRECTORY, EXCLUDES)
+final_results = create_final_results(results)
 
 # Print the sorted dictionary
-for key, value in sorted_dict.items():
+for key, value in final_results.items():
+    print("- Name:", key)
+    print("  Count: ", value["count"])
+    for i in value["paths"]:
+        print("     ", i)
     print()
-    print(key, "|", value)
